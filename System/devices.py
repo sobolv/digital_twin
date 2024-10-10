170421bb39d62baa171c66900df50fa97d796fe0
Sobol Valeriia <lera.sobol.26@gmail.com>
2024-10-11 00:24:16 +0300

    Add power functions to several devices

--- System/devices.py
+++ System/devices.py
@@ -1,19 +1,20 @@
+import random
+
+from System import config
+
 class HairDryer:
     POWER = 2100
 
     def power_on(self, temperature: int, speed: int):
-        if temperature == 1 and speed == 1:
-            return 700
-        elif temperature == 2 and speed == 1:
-            return 1000
-        elif temperature == 3 and speed == 1:
-            return 1500
-        elif temperature == 1 and speed == 2:
-            return 900
-        elif temperature == 2 and speed == 2:
-            return 1500
-        elif temperature == 3 and speed == 2:
-            return 2100
+        power_map = {
+            (1, 1): 700,
+            (2, 1): 1000,
+            (3, 1): 1500,
+            (1, 2): 900,
+            (2, 2): 1500,
+            (3, 2): self.POWER,
+        }
+        return power_map.get((temperature, speed))/config.time_delta
 
 
 
@@ -22,18 +23,41 @@ class Fridge:
     NOMINAL_POWER = 476
     MIN_START_POWER = 500
     MAX_START_POWER = 1000
-    MIN_START_TIME = 0.00028
-    MAX_START_TIME = 0.00056
+    MIN_START_TIME = 1 #seconds
+    MAX_START_TIME = 2 #seconds
+
+    def __init__(self):
+        self.current_time = 0
+
+    def power_on(self):
+        if self.current_time <= self.MAX_START_TIME:
+            power = random.randint(self.MIN_START_POWER, self.MAX_START_POWER) / config.time_delta
+        else:
+            power = self.NOMINAL_POWER / config.time_delta
+
+        if config.time_delta == 60:
+            self.current_time += 60
+        elif config.time_delta == 3600:
+            self.current_time += 1
+        else:
+            self.current_time += 3600
+        return power
 
 
 class Lamp:
     MODEL = "IKEA ARSTID + LED lamp"
     NOMINAL_POWER = 8
 
+    def power_on(self):
+        return self.NOMINAL_POWER/config.time_delta
+
 
 class Microwave:
     MODEL = "Samsung MW3000AM MS20A3010AL"
     MODE = {120, 250, 380, 500, 700}
+
+    def power_on(self, mode: int):
+        return mode/config.time_delta
 
 
 class TV:
@@ -42,15 +66,30 @@ class TV:
     NOMINAL_POWER = 108
     STAND_BY_MODE_POWER = 0.5
 
+    def power_on(self):
+        return random.randint(self.NOMINAL_POWER, self.MAX_POWER)/config.time_delta
+
 
 class Kettle:
     MODEL = "Philips Daily Collection HD9350/90"
     NOMINAL_POWER = 2200
+    STAND_BY_MODE_POWER = 0.5
     start_temperature = 20
 
-    def power_on(self, start_temperature: int, amount_of_water: int):
-        time = 4,186 * amount_of_water * (100 - start_temperature)/self.NOMINAL_POWER
-        return time
+    def __init__(self, amount_of_water: int, start_temperature: int):
+        self.time = (4.186 * amount_of_water * (100 - start_temperature))/self.NOMINAL_POWER
+        self.current_time = 0
+
+    def power_on(self):
+        if self.current_time < self.time:
+            if config.time_delta == 60:
+                self.current_time += 60
+            elif config.time_delta == 3600:
+                self.current_time += 1
+            elif config.time_delta == 1:
+                self.current_time += 3600
+            return self.NOMINAL_POWER/config.time_delta
+        return 0
 
 
 class Phone:
